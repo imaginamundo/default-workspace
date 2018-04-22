658c09b223be3a317793a54125a21e280f8e99a6
Diogo Ferreira Fernandes <diogo.ff@icloud.com>
2018-04-21 22:15:43 -0300

    Remove comments on helper

--- _helpers.js
+++ _helpers.js
@@ -3,18 +3,6 @@ const path = require('path');
 
 module.exports = {
     sweepFolder: (folderPath) => {
-        // To do:
-        // * Get only files from specific extension (ex.: '.js' or ['.js', '.jsx']);
-        // * Exclude file from list (ex.: 'index.js', or ['index.js', 'bundle.js']);
-        // * Paramethers as string or object, exemple:
-        // sweepFolder('path/to/folder');
-        // or
-        // sweepFolder({
-        //     path: 'path/to/folder',
-        //     extension: ['.js', '.jsx'],
-        //     exclude: ['index.js', 'bundle.js'] 
-        // });
-
         const files = [];
 
         readdirSync(folderPath).forEach(file => {
