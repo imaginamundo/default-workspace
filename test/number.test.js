91e057703b6a7eb90c143b9dfb5ce8adbdff75bc
Diogo Ferreira Fernandes <diogo.ff@icloud.com>
2018-04-23 19:51:11 -0300

    Add new test case

--- test/number.test.js
+++ test/number.test.js
@@ -4,5 +4,7 @@ describe('String', () => {
     it('Format currency', () => {
         expect(formatCurrency(20))
             .toBe('20,00');
+        expect(formatCurrency(1000.2))
+            .toBe('1.000,20');
     });
 });
